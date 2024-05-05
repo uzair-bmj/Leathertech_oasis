f8ab6fcbd983860bb146adf20c6f665beb9c117d
uzair-bmj <uzairbmj@gmail.com>
2024-05-05 13:01:43 +0500

    profile responsive

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -10,7 +10,7 @@ export default function Profile() {
   const found = users.find((items) => { return items.Email == profile })
 
   useEffect(() => {
-    if (found) {
+    if (found) {  
       setname(found)
     }
     else {
@@ -23,12 +23,12 @@ export default function Profile() {
 
   return (
     <>
-      <div className='bg-gray-300 h-[100vh]'>
-        <div className='w-80vw flex flex-col justify-center items-center '>
+      <div className='bg-gray-300 h-[100vh] '>
+        <div className=' flex flex-col justify-center items-center '>
           <div className=' rounded-full mt-32 px-10 py-10 cursor-pointer hover:bg-gray-500 hover:scale-110 transition-all delay-100' style={{ border: "1px solid gray" }}>
             <i className="fa-regular fa-user cursor-pointer " style={{ fontSize: "80px" }}></i>
           </div>
-          <div className='mt-16 flex justify-between w-[30vw]'>
+          <div className='mt-16 flex justify-between w-[80vw] md:w-[30vw]'>
             <div className='flex flex-col gap-8'>
               <h1 className='text-xl font-bold'>Full Name</h1>
               <h1 className='text-xl font-bold'>Email</h1>
